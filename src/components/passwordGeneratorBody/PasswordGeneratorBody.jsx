951f24d9f0f1143b91a97ddba248dd9bc3d8eb13
Sem31 <31kamleshprajapat@gmail.com>
2024-12-14 04:44:15 +0530

    body

--- src/components/passwordGeneratorBody/PasswordGeneratorBody.jsx
+++ src/components/passwordGeneratorBody/PasswordGeneratorBody.jsx
@@ -1,3 +1,34 @@
+import s from "./style.module.css";
+
 export function PasswordGeneratorBody() {
-  return <div>Password Generator Body</div>;
+  return (
+    <form>
+      <div className={s.grid_container}>
+        <div className={s.grid_item}>
+          Size
+          <select>
+            <option value={5}>5</option>
+            <option value={10}>10</option>
+            <option value={15}>15</option>
+            <option value={20}>20</option>
+          </select>
+        </div>
+        <div className={s.grid_item}>
+          Uppercase
+          <input type="checkbox" />
+        </div>
+        <div className={s.grid_item}>
+          Lowercase
+          <input type="checkbox" />
+        </div>
+        <div className={s.grid_item}>
+          Specials
+          <input type="checkbox" />
+        </div>
+      </div>
+      <div className={s.btn_container}>
+        <button className={s.btn_generator}>GENERATE</button>
+      </div>
+    </form>
+  );
 }
