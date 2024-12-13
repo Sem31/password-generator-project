79d15d762ef5267cb4f5c8e7cedfc275a3120bf8
Sem31 <31kamleshprajapat@gmail.com>
2024-12-14 04:54:47 +0530

    footer

--- src/components/passwordGeneratorFooter/PasswordGeneratorFooter.jsx
+++ src/components/passwordGeneratorFooter/PasswordGeneratorFooter.jsx
@@ -1,5 +1,11 @@
 import s from "./style.module.css";
+import copyPaste from "../../assets/icons/copy-paste.svg";
 
 export function PasswordGeneratorFooter() {
-  return <div className={s.root}>Password Generator Footer</div>;
+  return (
+    <div className={s.root}>
+      <div>Password Generator</div>
+      <img src={copyPaste} className={s.img} />
+    </div>
+  );
 }
