1a4c8d4674dac99dbd29b6ef676aed384abaea41
Sem31 <31kamleshprajapat@gmail.com>
2024-12-14 04:27:11 +0530

    Header

--- src/components/passwordGeneratorHeader/PasswordGeneratorHeader.jsx
+++ src/components/passwordGeneratorHeader/PasswordGeneratorHeader.jsx
@@ -1,3 +1,11 @@
+import lock from "../../assets/icons/lock-icon.svg";
+import s from "./style.module.css";
+
 export function PasswordGeneratorHeader() {
-  return <div>Password Generator Header</div>;
+  return (
+    <div className={s.root}>
+      <div className={s.title}>Password Generator</div>
+      <img src={lock} className={s.icon} alt="Lock icon" />
+    </div>
+  );
 }
